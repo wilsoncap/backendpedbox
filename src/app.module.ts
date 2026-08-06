a2c7751e8943170e5ca83915dc916d866c20bd9d
wilson capacho <wiston.98@hotmail.com>
2026-08-06 18:31:58 -0500

    configiracion en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './config/validation/env.validation';
+import { typeOrmConfig } from './database/config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
